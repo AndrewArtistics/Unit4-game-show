b1a2b202cb59516c3f61dfc8716a41f6607f14c9
AndrewArtistics <50923847+AndrewArtistics@users.noreply.github.com>
2019-08-31 13:35:11 -0700

    code added

--- js/app.js
+++ js/app.js
@@ -4,3 +4,13 @@ Project 4 - OOP Game App
 app.js 
 Andy Tuinstra
 **********/
+let game = null;
+
+$('#btn__reset').on('click' , function (){
+    game = new Game();
+    game.startGame();
+});
+
+$('.key').on('click', function(e) {
+    game.handleInteraction(e.target);
+});
